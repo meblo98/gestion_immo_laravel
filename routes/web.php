21d2627a29f705540fdeec16bf746fff8a65a9db
ANNA MARONE <annamarone72@gmail.com>
2024-06-03 17:47:06 +0000

    fonctionnlité ajout et affichage des commentaires

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CommentaireController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[CommentaireController::class,'commentaire']);
+Route::get('/creation',[CommentaireController::class,'ajouter'])->name('commentaire.ajouter');
