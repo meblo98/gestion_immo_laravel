f2e1c0c67b61367d588e8afa6434f3ba852945c5
meblo98 <lomouhamedelbachir@gmail.com>
2024-06-03 17:55:58 +0000

    finalisation du CRUD bien

--- app/Http/Controllers/BienController.php
+++ app/Http/Controllers/BienController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bien;
 use Illuminate\Http\Request;
 
 class BienController extends Controller
 {
-    //
+    public $biens;
+    public function __construct(){
+        $this->biens = new Bien();
+
+    }
+
+    public function index(){
+        $biens = Bien::all();
+        return view('bien.index', ['biens' => $biens]);
+    }
+
+    public function detail($id)
+    {
+        $biens = Bien::find($id);
+        return view('bien.detail', compact('biens'));
+    }
+    
+
+    public function ajout(){
+        return view ('bien.ajout');
+    }
+
+    public function creation(Request $request){
+        $this->biens->create($request->all());
+        return redirect()->back();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BienController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// routes des articles
+
+Route::get('/', [BienController::class, 'index']);
+Route::get('/detail/{id}', [BienController::class, 'detail'])->name('detail');
+
+
+Route::get('/ajout', [BienController::class, 'ajout'])->name('ajout');
+Route::post('/ajout/creation', [BienController::class, 'creation'])->name('creation');
