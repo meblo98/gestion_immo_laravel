ae557cf4ddc191c76deaf6a810803068367fcbd8
ANNA MARONE <annamarone72@gmail.com>
2024-06-04 06:18:13 +0000

    fonctionnalité connexion et deconnetion d'un utilisateur

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -13,6 +14,24 @@ class AuthController extends Controller
     }
     //
     public function creerCompte(Request $request){
+        $request->validate([
+            'nom' => 'required|string|max:255',
+            'prenom' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'telephone' => 'required|string|max:15',
+            'password' => 'required|string|min:8|confirmed',
+        ],
+         [
+            'nom.required' => 'Le nom est obligatoire.',
+            'prenom.required' => 'Le prénom est obligatoire.',
+            'email.required' => 'L\'email est obligatoire.',
+            'email.email' => 'L\'email doit être une adresse valide.',
+            'email.unique' => 'Cet email est déjà utilisé.',
+            'telephone.required' => 'Le numéro de téléphone est obligatoire.',
+            'password.required' => 'Le mot de passe est obligatoire.',
+            'password.min' => 'Le mot de passe doit contenir au moins 8 caractères.',
+            'password.confirmed' => 'La confirmation du mot de passe ne correspond pas.',
+        ]);
         $user=new User();
         $user->nom=$request->nom;
         $user->prenom=$request->prenom;
@@ -20,6 +39,31 @@ class AuthController extends Controller
         $user->telephone=$request->telephone;
         $user->password=Hash::make($request->password);
         $user->save();
-        return back()->with('success','compte créer avec sucess');
+        return view('authentification.connexion')->with('success','compte créer avec sucess');
+    }
+    public function connexion(){
+        return view('authentification.connexion');
+    }
+    public function seconnexion(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+        $credentials=[
+            'email' => $request->email,
+            'password'=>$request->password,
+        ];
+        if(Auth::attempt($credentials)){
+           return redirect()->route('ajout')->with('success','Connexion');
+
+        }else{
+            return back()->with('error','Email ou mots de passe incorrect');
+        }
+       
+        
+    }
+    public function deconnexion(){
+        Auth::logout();
+        return view('authentification.connexion');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ Route::get('/creation',[CommentaireController::class,'ajouter'])->name('commenta
 Route::get('/compte',[AuthController::class,'compte']);
 Route::post('/compte',[AuthController::class,'creerCompte'])->name('User.creerCompte');
 
+Route::get('/connexion',[AuthController::class,'connexion']);
+Route::post('/connexion',[AuthController::class,'seconnexion'])->name('User.seconnexion');
+Route::delete('/deconnexion',[AuthController::class,'deconnexion'])->name('User.deconnexion');
+
+
 
 // routes des articles
 
@@ -21,6 +26,7 @@ Route::get('/detail/{id}', [BienController::class, 'detail'])->name('detail');
 
 
 Route::get('/ajout', [BienController::class, 'ajout'])->name('ajout');
+
 Route::post('/ajout/creation', [BienController::class, 'creation'])->name('creation');
 
 Route::get('/commentaire',[CommentaireController::class,'commentaire']);
